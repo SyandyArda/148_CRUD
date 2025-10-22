9d21523859cb0be3b07bfc8dcb271140d23fdc5b
SyandyArda <syandi.2901@gmail.com>
2025-10-22 11:08:18 +0700

    menambah api get dan post

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+const db = mysql.createPool({
+    host: 'localhost',
+    port: 3309,
+    user: 'root',
+    password: 'Arsyan290105', // Sesuaikan dengan password Anda
+    database: 'mahasiswa'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:' + err.stack);
+        return;
+    }
+    console.log('Connected Succesfully');
+});
+
+app.get('/mahasiswa', (req, res) => {
+    db.query('SELECT *from biodata', (err, results) => {
+        if (err) {
+            console.error("Error executing query:", err.stack);
+            return res.status(500).json('Error fetching mahasiswa');
+        }
+        res.json({ message: "Data berhasil diambil", data: results });
+    });
+});
+
+
+app.post('/mahasiswa', (req, res) => {
+    const {nama, alamat, agama} = req.body;
+
+    if(!nama || !alamat || !agama) {
+        return.res.status(400).json({message: "Nama, alamat, dan agama harus diisi!."});
+    }
+
+    db.query(
+        "INSERT INTO biodata (nama, alamat, agama) VALUES (?, ?, ?)",
+        [nama, alamat, agama],
+        (err, results)
+    )
